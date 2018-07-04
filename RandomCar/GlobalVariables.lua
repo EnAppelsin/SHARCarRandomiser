0d2608bc6ca54f9490befa2a97375e73a05941b3
Gadiie <40698545+Gadiie@users.noreply.github.com>
2018-07-04 17:30:37 +0100

    Changes to random stats (#30)
    * Changes to random stats
    - Allows the player to decide the limits for each aspect of driving that is randomised
    - Mod settings are now in separate pages due to the large number of options

--- RandomCar/GlobalVariables.lua
+++ RandomCar/GlobalVariables.lua
@@ -24,3 +24,67 @@ conversations = nil
 dialogsptend = nil
 
 cartunespt = nil
+
+--Random Stat Min/Max Variables
+if GetSetting("RandomStats") then
+--Mass Variables
+	MinMass = GetSetting("StatMinMass")
+	MaxMass = GetSetting("StatMaxMass")
+	if MaxMass < MinMass then
+		MaxMass = MinMass
+	end
+--Gas Scale Variables
+	MinGas = GetSetting("StatMinGas")
+	MaxGas = GetSetting("StatMaxGas")
+	if MaxGas < MinGas then
+		MaxGass = MinGas
+	end
+--Slip Gas Scale Variables
+	MinSlipGas = GetSetting("StatMinSlipGasScale")
+	MaxSlipGas = GetSetting("StatMaxSlipGasScale")
+	if MaxSlipGas < MinSlipGas then
+		MaxSlipGas = MinSlipGas
+	end
+-- Break Gas Scale Variables
+	MinBreakGasScale = GetSetting("StatMinBreakGasScale")
+	MaxBreakGasScale = GetSetting("StatMaxBreakGasScale")
+	if MaxBreakGasScale < MinBreakGasScale then
+		MaxBreakGasScale = MinBreakGasScale
+	end
+-- Top Speed Variables
+	MinSpeed = GetSetting("StatMinSpeed")
+	MaxSpeed = GetSetting("StatMaxSpeed")
+	if MaxSpeed < MinSpeed then
+		MaxSpeed = MinSpeed
+	end
+-- Wheel Turn Angle
+	MinTurnAngle = GetSetting("StatMinAngle")
+	MaxTurnAngle = GetSetting("StatMaxAngle")
+	if MaxTurnAngle < MinTurnAngle then
+		MaxTurnAngle = MinTurnAngle
+	end
+-- Wheel Grip Variables
+	MinGrip = GetSetting("StatMinGrip")
+	MaxGrip = GetSetting("StatMaxGrip")
+	if MaxGrip < MinGrip then
+		MaxGrip = MinGrip
+	end
+-- Steering Variables
+	MinSteering = GetSetting("StatMinSteering")
+	MaxSteering = GetSetting("StatMaxSteering")
+	if MaxSteering < MinSteering then
+		MaxSteering = MinSteering
+	end
+-- SlipSteering Variables
+	MinSlipSteering = GetSetting("StatMinSlipSteering")
+	MaxSlipSteering = GetSetting("StatMaxSlipSteering")
+	if MaxSlipSteering < MinSlipSteering then
+		MaxSlipSteering = MinSlipSteering
+	end
+-- HP Variables
+	MinHP = GetSetting("StatMinHP")
+	MaxHP = GetSetting("StatMaxHP")
+	if MaxHP < MinHP then
+		MaxHP = MinHP
+	end
+end
