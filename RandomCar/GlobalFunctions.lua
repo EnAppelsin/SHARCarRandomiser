7f4c73c60404a8adf4752b0d3168ba1d4bf7e3b2
EnAppelsin <39798849+EnAppelsin@users.noreply.github.com>
2018-07-05 16:24:10 +0100

    Move model handling function to GlobalFunctions so it's loaded once

--- RandomCar/GlobalFunctions.lua
+++ RandomCar/GlobalFunctions.lua
@@ -84,3 +84,68 @@ end
 function p3d_debug(message)
     --print(message)
 end
+
+function ReplaceCharacterSkinSkel(Original, Replace)
+    -- Copy textures over
+    local Textures = ""
+	for position, length in FindSubchunks(Replace, TEXTURE_CHUNK) do
+		Textures = Textures .. Replace:sub(position, position + length - 1)
+	end
+
+    -- Copy shaders over
+	local Shaders = ""
+	local ShaderList = {}
+	for position, length in FindSubchunks(Replace, SHADER_CHUNK) do
+		local ShaderName = GetP3DString(Replace, position + 12)
+		ShaderList[ShaderName] = true
+		local Shader =  Replace:sub(position, position + length - 1)
+		Shaders = Shaders .. Shader
+	end
+
+    -- Remove clashing shaders
+	local Adjust = 0
+	for position, length in FindSubchunks(Original, SHADER_CHUNK) do
+		local ShaderName = GetP3DString(Original, position + 12 - Adjust)
+		if ShaderList[ShaderName] then
+			p3d_debug("Removing clashing shader " .. ShaderName)
+			Original = RemoveString(Original, position - Adjust, position + length - Adjust)
+		end
+	end
+    
+    -- Load new skeleton
+    local SKIndex, SKLength = FindSubchunk(Replace, SKELETON_CHUNK)
+    local NewSkel = Replace:sub(SKIndex, SKIndex + SKLength - 1)
+    
+    -- Load new skin
+    local SNIndex, SNLength = FindSubchunk(Replace, SKIN_CHUNK)
+    local NewSkin = Replace:sub(SNIndex, SNIndex + SNLength - 1)
+    
+    -- Find Original Skeleton, Remove it
+    SKIndex, SKLength = FindSubchunk(Original, SKELETON_CHUNK)
+    local SkelName, SkelNLength = GetP3DString(Original, SKIndex + 12)
+    Original = RemoveString(Original, SKIndex, SKIndex + SKLength)
+    
+    -- Find Original Skin
+    SNIndex, SNLength = FindSubchunk(Original, SKIN_CHUNK)
+    local SkinName, SkinNLength = GetP3DString(Original, SNIndex + 12)
+    
+    -- Change names and update lengths
+    NewSkel, SkelDelta, OSName = SetP3DString(NewSkel, 13, SkelName)
+    NewSkel = AddP3DInt4(NewSkel, 5, SkelDelta)
+    NewSkel = AddP3DInt4(NewSkel, 9, SkelDelta)
+    NewSkin, SkinDelta, OS2Name = SetP3DString(NewSkin, 13, SkinName)
+    local SkelNameIndex = SkinName:len() + 18
+    NewSkin, SkinDelta2, OS3Name = SetP3DString(NewSkin, SkelNameIndex, SkelName)
+    NewSkin = AddP3DInt4(NewSkin, 5, SkinDelta + SkinDelta2)
+    NewSkin = AddP3DInt4(NewSkin, 9, SkinDelta + SkinDelta2)
+    
+    p3d_debug(OSName, "->", SkelName, OS2Name, "->", SkinName, OS3Name, "->", SkelName)
+    
+    -- Add to original model
+    Original = Original:sub(1, SNIndex - 1) .. Textures .. Shaders .. NewSkel .. NewSkin .. Original:sub(SNIndex + SNLength)
+    
+    -- Update file length
+    Original = SetP3DInt4(Original, 9, Original:len())
+    
+    return Original
+end
