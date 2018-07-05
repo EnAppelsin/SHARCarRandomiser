839204aa7700825aa4d48c9724cac67843b6dffe
EnAppelsin <39798849+EnAppelsin@users.noreply.github.com>
2018-07-05 17:45:31 +0100

    RandomCouch Cleanup
    - Use local variables everywhere (hopefully)
    - Use new P3D handling functions to simplify code

--- RandomCar/RandomCouch.lua
+++ RandomCar/RandomCouch.lua
@@ -72,50 +72,39 @@ if GetSetting("RandomCouch") then
 	p3d_debug("> New skin name is " .. NewSkinName)
 	-- Update the skeleton
 	local SNIndex = SNLength + 18
-	SNLength = String1ToInt(NewSkin:sub(SNIndex, SNIndex))
-	local SkinSkelName, SNLength = GetP3DString(NewSkin, SNIndex)
-	NewSkin = NewSkin:sub(1, SNIndex - 1) .. MOTION_ROOT_LABEL .. NewSkin:sub(SNIndex + SNLength + 1)
-	local HeaderLength = String4ToInt(NewSkin:sub(5, 8))
-	HeaderLength = HeaderLength - SNLength + 12
-	local HeaderBytes = IntToString4(HeaderLength)
-	NewSkin = NewSkin:sub(1, 4) .. HeaderBytes .. NewSkin:sub(9)
-	LengthBytes = IntToString4(NewSkin:len())
-	NewSkin = NewSkin:sub(1, 8) .. LengthBytes .. NewSkin:sub(13)
+    local Delta
+    NewSkin, Delta = SetP3DString(NewSkin, SNIndex, MOTION_ROOT_LABEL)
+    NewSkin = AddP3DInt4(NewSkin, 5, Delta)
+    NewSkin = AddP3DInt4(NewSkin, 9, Delta)
+    
 
 	-- Find the original skeleton
-	SKIndex, SKLength = FindSubchunk(Original, SKELETON_CHUNK)
+	local SKIndex, SKLength = FindSubchunk(Original, SKELETON_CHUNK)
 	p3d_debug("Original Skeleton as at " .. SKIndex)
 
 	-- Load the skeleton from the replacement
-	NewSkelL, Length = FindSubchunk(Replace, SKELETON_CHUNK)
+	local NewSkelL, Length = FindSubchunk(Replace, SKELETON_CHUNK)
 	p3d_debug("New skeleton as at " .. NewSkelL)
-	NewSkel = Replace:sub(NewSkelL, NewSkelL + Length - 1)
+	local NewSkel = Replace:sub(NewSkelL, NewSkelL + Length - 1)
 
 	-- Update the skeleton name
-	OrigSkelName, SNLength = GetP3DString(NewSkel, 13)
+	local OrigSkelName, SNLength = GetP3DString(NewSkel, 13)
 	p3d_debug("Original skeleton name is " .. OrigSkelName)
-	NewSkel = NewSkel:sub(1, 12) .. MOTION_ROOT_LABEL .. NewSkel:sub(13 + SNLength + 1)
-	HeaderLength = String4ToInt(NewSkel:sub(5, 8))
-	HeaderLength = HeaderLength - SNLength + 12
-	HeaderBytes = IntToString4(HeaderLength)
-	NewSkel = NewSkel:sub(1, 4) .. HeaderBytes .. NewSkel:sub(9)
-	LengthBytes = IntToString4(NewSkel:len())
-	NewSkel = NewSkel:sub(1, 8) .. LengthBytes .. NewSkel:sub(13)
+	NewSkel, Delta =  SetP3DString(NewSkel, 13, MOTION_ROOT_LABEL)
+	NewSkel = AddP3DInt4(NewSkel, 5, Delta)
+    NewSkel = AddP3DInt4(NewSkel, 9, Delta)
 
 	p3d_debug("Replacing skeleton and adding skin to couch model")
 	Original = Original:sub(1, SKIndex - 1) .. Textures .. Shaders .. NewSkel .. NewSkin .. Original:sub(SKIndex + SKLength)
 
+	local CDIndex, CDLength = FindSubchunk(Original, COMP_DRAW_CHUNK)
+	local CD = Original:sub(CDIndex, CDIndex + CDLength - 1)
+
+	local CDSLIndex, CDSLLength = FindSubchunk(CD, COMP_DRAW_SKIN_LIST_SUBCHUNK)
+	local CDSL = CD:sub(CDSLIndex, CDSLIndex + CDSLLength - 1)
 
 
-
-	CDIndex, CDLength = FindSubchunk(Original, COMP_DRAW_CHUNK)
-	CD = Original:sub(CDIndex, CDIndex + CDLength - 1)
-
-	CDSLIndex, CDSLLength = FindSubchunk(CD, COMP_DRAW_SKIN_LIST_SUBCHUNK)
-	CDSL = CD:sub(CDSLIndex, CDSLIndex + CDSLLength - 1)
-
-
-	positions = {}
+	local positions = {}
 	for i in FindSubchunks(CDSL, COMP_DRAW_SKIN_SUBCHUNK) do
 		table.insert(positions, i)
 	end
@@ -124,42 +113,32 @@ if GetSetting("RandomCouch") then
 
 	for i = 1,#positions-1 do
 		p3d_debug("Composite drawable skin at " .. positions[i])
-		LengthStr = Original:sub(CDIndex + CDSLIndex + positions[i] + 8 - 2 - Adjust, CDIndex + CDSLIndex + positions[i] + 11 - Adjust - 2)
-		Length = String4ToInt(LengthStr)
+		local LengthStr = Original:sub(CDIndex + CDSLIndex + positions[i] + 8 - 2 - Adjust, CDIndex + CDSLIndex + positions[i] + 11 - Adjust - 2)
+		local Length = String4ToInt(LengthStr)
 		Original = RemoveString(Original, CDIndex + CDSLIndex + positions[i] - 2 - Adjust, CDIndex + CDSLIndex + positions[i] + Length - Adjust - 2)
 		Adjust = Adjust + Length
 		p3d_debug("Removed the composite drawable skin")
 	end
 
 	-- Patch the last composite drawable skin
-	LastCDS = CDIndex + CDSLIndex +  positions[#positions] - Adjust - 2
+	local LastCDS = CDIndex + CDSLIndex +  positions[#positions] - Adjust - 2
 
 	p3d_debug("> Changing comp drawable name to " .. NewSkinName)
     Original, Delta = SetP3DString(Original, LastCDS + 12, NewSkinName)
-
-	HeaderLength = String4ToInt(Original:sub(LastCDS + 4, LastCDS + 7))
-	HeaderLength = HeaderLength + Delta
-	HeaderBytes = IntToString4(HeaderLength)
-	Original = Original:sub(1, LastCDS + 3) .. HeaderBytes .. Original:sub(LastCDS + 8)
-	ChunkLength = String4ToInt(Original:sub(LastCDS + 8, LastCDS + 11))
-	LengthBytes = IntToString4(ChunkLength + Delta)
-	Original = Original:sub(1, LastCDS + 7) .. LengthBytes .. Original:sub(LastCDS + 12)
+    Original = AddP3DInt4(Original, LastCDS + 4, Delta)
+    Original = AddP3DInt4(Original, LastCDS + 8, Delta)
 
 	Adjust = Adjust - Delta
 
 	p3d_debug("Patching composite drawable skin list")
-	LengthBytes = IntToString4(CDSLLength - Adjust)
-	Original = Original:sub(1, CDIndex + CDSLIndex + 7 - 1) .. LengthBytes .. Original:sub(CDIndex + CDSLIndex + 12 - 1)
-	CountBytes = IntToString4(1)
-	Original = Original:sub(1, CDIndex + CDSLIndex + 11 - 1) .. CountBytes .. Original:sub(CDIndex + CDSLIndex + 16 - 1)
+    Original = SetP3DInt4(Original, CDIndex + CDSLIndex + 8 - 1, CDSLLength - Adjust)
+    Original = SetP3DInt4(Original, CDIndex + CDSLIndex + 12 - 1, 1)
 
 	p3d_debug("Patching composite drawable length")
-	LengthBytes = IntToString4(CDLength - Adjust)
-	Original = Original:sub(1, CDIndex + 7) .. LengthBytes .. Original:sub(CDIndex + 12)
+    Original = SetP3DInt4(Original, CDIndex + 8, CDLength - Adjust)
 
 	p3d_debug("Patching total file length")
-	LengthBytes = IntToString4(Original:len())
-	Original = Original:sub(1, 8) .. LengthBytes .. Original:sub(13)
+    Original = SetP3DInt4(Original, 9, Original:len())
 
 	print ("Couch modified with model from " .. ReplacePath)
 
