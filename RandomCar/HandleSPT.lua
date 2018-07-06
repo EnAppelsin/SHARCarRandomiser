e818640395263d5ee3aa9e8d7d8827fef525c03e
Proddy3 <tehmcterrorist@gmail.com>
2018-07-06 12:58:03 +0100

    v1.9
    Rename to Randomiser fully

--- RandomCar/HandleSPT.lua
+++
@@ -1,5 +0,0 @@
-local Path = "/GameData/" .. GetPath();
-
-if string.match(Path, "car_tune%.spt") then
-	Output(cartunespt)
-end
---
+++ Randomiser/HandleSPT.lua
@@ -0,0 +1,5 @@
+local Path = "/GameData/" .. GetPath();
+
+if string.match(Path, "car_tune%.spt") then
+	Output(cartunespt)
+end
