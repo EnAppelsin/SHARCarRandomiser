e9bef8819b5f689999159d9ef11d28ff7c5bc33b
EnAppelsin <39798849+EnAppelsin@users.noreply.github.com>
2018-07-14 21:42:26 +0100

    Move ModVersion variable to GlobalVariables.lua

--- Randomiser/CustomFiles.lua
+++ Randomiser/CustomFiles.lua
@@ -3,7 +3,6 @@ dofile(ModPath .. "/GlobalArrays.lua")
 dofile(ModPath .. "/GlobalVariables.lua")
 dofile(ModPath .. "/GlobalFunctions.lua")
 
-ModVersion = ReadFile(ModPath .. "/Meta.ini"):match("Version=(.-)\r\n")
 print("Randomiser v" .. ModVersion)
 print("Randomiser: Using " .. RandomCarPoolN .. " cars")
 print("Randomiser: Using " .. RandomPedPoolN .. " pedestrians")
