b3087a24db905de7cc221d536d21414bdaf80a3a
EnAppelsin <39798849+EnAppelsin@users.noreply.github.com>
2018-07-31 21:08:40 +0100

    Delete HandleRSD.lua

--- Randomiser/HandleRSD.lua
+++
@@ -1,8 +0,0 @@
-local Path = "/GameData/" .. GetPath()
-if SettingRandomDialogue and RandomDialoguePoolN > 0 then
-	local RedirectPath = RandomDialoguePool[math.random(RandomDialoguePoolN)]
-
-	DebugPrint("Redirecting " .. Path .. " to " .. RedirectPath)
-
-	Redirect(RedirectPath)
-end
