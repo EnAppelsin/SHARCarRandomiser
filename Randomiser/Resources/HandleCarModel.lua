ef1e23d43c41c952a302c92a96c3e9710d8b0039
Proddy <tehmcterrorist@gmail.com>
2018-08-07 10:39:58 +0100

    Add check for player car or traffic car
    Sets camera index to 96 if player or traffic car

--- Randomiser/Resources/HandleCarModel.lua
+++ Randomiser/Resources/HandleCarModel.lua
@@ -3,11 +3,13 @@ if SettingCustomCars then
 	local carName = Path:match("[\\/]cars[\\/](.-)%.p3d")
 	for i = 1, #CustomCarPool do
 		if CustomCarPool[i]:lower() == carName:lower() then
-			--TODO: Write SetCarCameraIndex and then replace Redirect
-			--local carData = ReadFile("/GameData/CustomCars/" .. CustomCarPool[i] .. "/" .. CustomCarPool[i] .. ".p3d")
-			--carData = SetCarCameraIndex(carData, 96)
-			--Output(carData)
-			Redirect("/GameData/CustomCars/" .. CustomCarPool[i] .. "/" .. CustomCarPool[i] .. ".p3d")
+			if RandomCarName:lower() == carName:lower() or ExistsInTbl(TrafficCars, carName, false) then
+				local carData = ReadFile("/GameData/CustomCars/" .. CustomCarPool[i] .. "/" .. CustomCarPool[i] .. ".p3d")
+				carData = SetCarCameraIndex(carData, 96)
+				Output(carData)
+			else
+				Redirect("/GameData/CustomCars/" .. CustomCarPool[i] .. "/" .. CustomCarPool[i] .. ".p3d")
+			end
 			break
 		end
 	end
