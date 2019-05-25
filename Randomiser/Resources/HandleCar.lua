32cd804b00727b7a7b242b98f00eb9558b8849fb
EnAppelsin <39798849+EnAppelsin@users.noreply.github.com>
2019-05-25 20:45:00 +0200

    Introduce a maximum cap on mission vehicle HP

--- Randomiser/Resources/HandleCar.lua
+++ Randomiser/Resources/HandleCar.lua
@@ -116,6 +116,10 @@ if SettingRandomMissionVehicles and MissionVehicles then
 					File = string.gsub(File, "SetHitPoints%(.-%);", "SetHitPoints(0.6);", 1)
 					DebugPrint("Boosting HP up from " .. HP .. " to 0.6 for " .. Path)
 				end
+				if HP and tonumber(HP) > 15 then
+					File = string.gsub(File, "SetHitPoints%(.-%);", "SetHitPoints(0.6);", 1)
+					DebugPrint("Capping HP from " .. HP .. " to 15 for " .. Path)
+				end
 			end
 		end
 	end
