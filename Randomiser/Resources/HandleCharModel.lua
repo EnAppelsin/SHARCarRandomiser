0128bc262db9f204c054274a682c7dd1bad23b45
Proddy3 <tehmcterrorist@gmail.com>
2020-06-22 21:20:14 +0100

    Pedestrian Swatches Fix
    - Added extra Debug Settings for characters
    - Fixed Pedestrian Swatches being randomised and breaking looks on some peds

--- Randomiser/Resources/HandleCharModel.lua
+++ Randomiser/Resources/HandleCharModel.lua
@@ -5,6 +5,12 @@ if Exists("/GameData/" .. Path, true, false) then
 	if Settings.RandomCharacter and OrigChar and (Path:match("art/chars/" .. OrigChar .. "_m%.p3d") or Path:match("art/chars/" .. OrigChar:sub(1,1) .. "_.-_m%.p3d")) then
 		local Original = ReadFile("/GameData/" .. Path)
 		local ReplacePath = GetRandomFromTbl(RandomCharP3DPool, false)
+		if Settings.UseDebugSettings and Exists("/GameData/RandomiserSettings/PlayerCharacter.txt", true, false) then
+			local staticName = ReadFile("/GameData/RandomiserSettings/PlayerCharacter.txt")
+			if staticName:len() > 0 then
+				ReplacePath = staticName
+			end
+		end
 		local Replace = ReadFile(ReplacePath)
 		
 		DebugPrint("Replacing \"" .. Path .. "\" with \"" .. ReplacePath .. "\"")
@@ -23,6 +29,12 @@ if Exists("/GameData/" .. Path, true, false) then
 					if Path:match("art/chars/" .. model .. "_m%.p3d") then
 						local Original = ReadFile("/GameData/" .. Path)
 						local ReplacePath = GetRandomFromTbl(RandomCharP3DPool, false)
+						if Settings.UseDebugSettings and Exists("/GameData/RandomiserSettings/MissionCharacter.txt", true, false) then
+							local staticName = ReadFile("/GameData/RandomiserSettings/MissionCharacter.txt")
+							if staticName:len() > 0 then
+								ReplacePath = staticName
+							end
+						end
 						local Replace = ReadFile(ReplacePath)
 						
 						DebugPrint("Replacing \"" .. Path .. "\" with \"" .. ReplacePath .. "\"")
@@ -43,6 +55,12 @@ if Exists("/GameData/" .. Path, true, false) then
 					if Path:match("art/chars/" .. model .. "_m%.p3d") then
 						local Original = ReadFile("/GameData/" .. Path)
 						local ReplacePath = GetRandomFromTbl(RandomCharP3DPool, false)
+						if Settings.UseDebugSettings and Exists("/GameData/RandomiserSettings/MissionCharacter.txt", true, false) then
+							local staticName = ReadFile("/GameData/RandomiserSettings/MissionCharacter.txt")
+							if staticName:len() > 0 then
+								ReplacePath = staticName
+							end
+						end
 						local Replace = ReadFile(ReplacePath)
 						
 						DebugPrint("Replacing \"" .. Path .. "\" with \"" .. ReplacePath .. "\"")
@@ -55,23 +73,67 @@ if Exists("/GameData/" .. Path, true, false) then
 				end
 			end
 		end
-		if Settings.RandomPedestrians and LevelCharacters and updated ~= true then
-			for i = 1, #LevelCharacters do
-				local model = LevelCharacters[i]
-				if model:len() > 6 then
-					model = model:sub(1, 6)
+		if Settings.RandomPedestrians then
+			if LevelPedestrians and updated ~= true then
+				for i = 1, #LevelPedestrians do
+					local model = LevelPedestrians[i]
+					if Path:match("art/chars/" .. model .. "%.p3d") then
+						local Original = ReadFile("/GameData/" .. Path)
+						local P3DFile = P3D.P3DChunk:new{Raw = Original}
+						local ShaderName
+						for idx, id in P3DFile:GetChunkIndexes(P3D.Identifiers.Shader) do
+							local ShaderChunk = P3D.ShaderP3DChunk:new{Raw = P3DFile:GetChunkAtIndex(idx)}
+							if P3D.CleanP3DString(ShaderChunk:GetTextureParameter("TEX")) == "char_swatches_lit.bmp" then
+								ShaderName = ShaderChunk.Name
+								ShaderChunk.Name = "new_swatches"
+								P3DFile:SetChunkAtIndex(idx, ShaderChunk:Output())
+								break
+							end
+						end
+						if ShaderName then
+							for idx in P3DFile:GetChunkIndexes(P3D.Identifiers.Skin) do
+								local SkinChunk = P3D.SkinP3DChunk:new{Raw = P3DFile:GetChunkAtIndex(idx)}
+								for opgIdx in SkinChunk:GetChunkIndexes(P3D.Identifiers.Old_Primitive_Group) do
+									local opg = P3D.OldPrimitiveGroupP3DChunk:new{Raw = SkinChunk:GetChunkAtIndex(opgIdx)}
+									if P3D.CleanP3DString(opg.ShaderName) == ShaderNAme then
+										opg.Name = "new_swatches"
+										opg.ShaderName = "new_swatches"
+										SkinChunk:SetChunkAtIndex(opgIdx, opg:Output())
+									end
+								end
+								P3DFile:SetChunkAtIndex(idx, SkinChunk:Output())
+							end
+							Output(P3DFile:Output())
+							updated = true						
+						end
+						break
+					end
 				end
-				if Path:match("art/chars/" .. model .. "_m%.p3d") then
-					local Original = ReadFile("/GameData/" .. Path)
-					local ReplacePath = GetRandomFromTbl(RandomCharP3DPool, false)
-					local Replace = ReadFile(ReplacePath)
-					
-					DebugPrint("Replacing \"" .. Path .. "\" with \"" .. ReplacePath .. "\"")
-					Original = ReplaceCharacterSkinSkel(Original, Replace)
-					
-					Output(Original)
-					updated = true
-					break
+			end
+			if LevelCharacters and updated ~= true then
+				for i = 1, #LevelCharacters do
+					local model = LevelCharacters[i]
+					if model:len() > 6 then
+						model = model:sub(1, 6)
+					end
+					if Path:match("art/chars/" .. model .. "_m%.p3d") then
+						local Original = ReadFile("/GameData/" .. Path)
+						local ReplacePath = GetRandomFromTbl(RandomCharP3DPool, false)
+						if Settings.UseDebugSettings and Exists("/GameData/RandomiserSettings/LevelCharacter.txt", true, false) then
+							local staticName = ReadFile("/GameData/RandomiserSettings/LevelCharacter.txt")
+							if staticName:len() > 0 then
+								ReplacePath = staticName
+							end
+						end
+						local Replace = ReadFile(ReplacePath)
+						
+						DebugPrint("Replacing \"" .. Path .. "\" with \"" .. ReplacePath .. "\"")
+						Original = ReplaceCharacterSkinSkel(Original, Replace)
+						
+						Output(Original)
+						updated = true
+						break
+					end
 				end
 			end
 		end
--- Randomiser/Resources/MissionModules/RandomPedestrians.lua
+++ Randomiser/Resources/MissionModules/RandomPedestrians.lua
@@ -10,7 +10,6 @@ if Settings.RandomPedestrians then
 	end
 	
 	function Level.RandomPedestrians(LoadFile, InitFile, Level, Path)
-		local Peds = ""
 		local TmpPedPool = {table.unpack(RandomPedPool)}
 		local groups = {}
 		for group in InitFile:gmatch("CreatePedGroup%s*%(%s*(%d)%s*%);") do
@@ -22,17 +21,33 @@ if Settings.RandomPedestrians then
 			local group = groups[i]
 			DebugPrint("Randomising group " .. group)
 			ret = ret .. "CreatePedGroup( " .. group .. " );\r\n"
-			for i = 1, 7 do
-				local pedName = GetRandomFromTbl(TmpPedPool, true)
-				if not TmpPedPool or #TmpPedPool == 0 then
-					TmpPedPool = {table.unpack(RandomPedPool)}
+			if Settings.UseDebugSettings and Exists("/GameData/RandomiserSettings/Pedestrians.txt", true, false) then
+				local contents = ReadFile("/GameData/RandomiserSettings/Pedestrians.txt")
+				if contents:len() > 0 then
+					for line in contents:gmatch("[^\r\n]+") do
+						local pipe = line:find("|", 1, true)
+						local pedName = pipe and line:sub(0, pipe - 1) or line
+						local count = pipe and tonumber(line:sub(pipe + 1)) or 1
+						ret = ret .. "AddPed(\"" .. pedName .. "\", " .. count .. ");\r\n"
+						if pedName:len() > 6 then
+							pedName = pedName:sub(1, 6)
+						end
+						LevelPedestrians[#LevelPedestrians + 1] = pedName .. "_m"
+					end
 				end
-				Peds = Peds .. pedName .. ", "
-				ret = ret .. "AddPed(\"" .. pedName .. "\", 1);\r\n"
-				if pedName:len() > 6 then
-					pedName = pedName:sub(1, 6)
+			end
+			if #LevelPedestrians == 0 then
+				for i = 1, 7 do
+					local pedName = GetRandomFromTbl(TmpPedPool, true)
+					if not TmpPedPool or #TmpPedPool == 0 then
+						TmpPedPool = {table.unpack(RandomPedPool)}
+					end
+					ret = ret .. "AddPed(\"" .. pedName .. "\", 1);\r\n"
+					if pedName:len() > 6 then
+						pedName = pedName:sub(1, 6)
+					end
+					LevelPedestrians[#LevelPedestrians + 1] = pedName .. "_m"
 				end
-				LevelPedestrians[#LevelPedestrians + 1] = pedName .. "_m"
 			end
 			ret = ret .. "ClosePedGroup( );"
 		end
@@ -43,7 +58,7 @@ if Settings.RandomPedestrians then
 		for npc in InitFile:gmatch("AddAmbientCharacter%s*%(%s*\"([^\n]-)\"") do
 			LevelCharacters[#LevelCharacters + 1] = npc
 		end
-		DebugPrint("Random pedestrians for level -> " .. Peds)
+		DebugPrint("Random pedestrians for level -> " .. table.concat(LevelPedestrians, ", "))
 		return LoadFile, InitFile
 	end
 end
