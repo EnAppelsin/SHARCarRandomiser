00b512183f200754a654460e1cd89498651d58a6
Duffhause <57455765+573V3N5@users.noreply.github.com>
2020-07-24 21:50:06 +0100

    Made rewards loaded from handler

---
+++ Randomiser/Resources/HandleRewards.lua
@@ -0,0 +1,4 @@
+--LoadP3DFile("art\\global0.p3d", "GMA_DEFAULT", "Global");
+Path = "/GameData/" .. GetPath();
+print("REWARD")
+Output(ReadFile(Path)..'\nLoadP3DFile("art\\global0.p3d", "GMA_DEFAULT", "Global");')
