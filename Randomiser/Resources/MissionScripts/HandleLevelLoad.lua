2d3a5a2d677836a28cd8e79a520cacbb4e559076
EnAppelsin <39798849+EnAppelsin@users.noreply.github.com>
2020-11-30 19:54:36 +0100

    Update HandleLevelLoad.lua

--- Randomiser/Resources/MissionScripts/HandleLevelLoad.lua
+++ Randomiser/Resources/MissionScripts/HandleLevelLoad.lua
@@ -1,11 +1,11 @@
 local Path = "/GameData/" .. GetPath();
+loading = true
 
 if Settings.IsSeeded then
 	Seed.HandleModulesLevel(Path)
 	return
 end
 
-loading = true
 if MissionModules.Level then
 	local level = tonumber(Path:match("level0(%d)"))
 	DebugPrint("NEW LEVEL LOAD: Level " .. level)
