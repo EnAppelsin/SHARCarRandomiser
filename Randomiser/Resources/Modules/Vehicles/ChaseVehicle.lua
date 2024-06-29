c1888af41f25747ab7f5178bfa33db37dff57c81
Proddy <josh@zhbot.org>
2024-06-29 12:07:54 +0100

    Added `RandomChaseVehicles`

---
+++ Randomiser/Resources/Modules/Vehicles/ChaseVehicle.lua
@@ -0,0 +1,35 @@
+local math_random = math.random
+
+local RandomChaseVehicle = Module("Random Chase Vehicle", "RandomChaseVehicle", 1)
+
+RandomChaseVehicle:AddLevelHandler(function(LevelNumber, LevelLoad, LevelInit)
+	local RandomVehicleIndex = math_random(CarCount)
+	local RandomVehicleP3D = CarP3DFiles[RandomVehicleIndex]
+	local RandomVehicleName = CarNames[RandomVehicleIndex]
+	
+	print("Setting chase car to: " .. RandomVehicleName)
+	
+	local origChase
+	local functions = LevelInit.Functions
+	for i=1,#functions do
+		local func = functions[i]
+		local name = func.Name:lower()
+		if name == "createchasemanager" then
+			origChase = func.Arguments[1]
+			func.Arguments[1] = RandomVehicleName
+			func.Arguments[2] = RandomVehicleName .. ".con"
+			break
+		end
+	end
+	
+	LevelLoad:SetAll("LoadP3DFile", 1, RandomVehicleP3D, "art\\cars\\" .. origChase .. ".p3d")
+	if Settings.RandomChaseAmount then
+		local amount = math_random(5)
+		print("Setting chase amount to: " .. amount)
+		LevelInit:SetAll("SetNumChaseCars", 1, amount)
+	end
+	
+	return true
+end)
+
+return RandomChaseVehicle
