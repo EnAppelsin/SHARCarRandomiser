bd3523eb60d37fd639ab23448b36ad54c0bba81f
Proddy <josh@zhbot.org>
2024-06-29 16:40:38 +0100

    Improved `SPT` handler

--- Randomiser/Resources/Modules/General/FixCartune.lua
+++ Randomiser/Resources/Modules/General/FixCartune.lua
@@ -3,24 +3,18 @@ local table_remove = table.remove
 local FixCartune = Module("Fix Cartune", nil, 0)
 
 FixCartune:AddSPTHandler("sound/scripts/car_tune.spt", function(Path, SPTFile)
-	local classes = SPTFile.Classes
-	for i=1,#classes do
-		local class = classes[i]
-		local classType = class.Type
-		if classType == "carSoundParameters" then
-			local variables = class.Variables
-			for j=#variables,1,-1 do
-				local variable = variables[j]
-				local variableName = variable.Name
-				if variableName == "SetEngineClipName" or variableName == "SetEngineIdleClipName" then
-					if variable.Arguments[1].Value == "tt" then
-						variable.Arguments[1].Value = "snake_car"
-					end
-				elseif variableName == "SetOverlayClipName" then
-					local clipName = variable.Arguments[1].Value
-					if clipName == "" or clipName == "generator" then
-						table_remove(variables, j)
-					end
+	for carSoundParameters in SPTFile:GetClasses("carSoundParameters") do
+		for variable, index in carSoundParameters:GetVariables(true) do
+			local name = variable.Name
+			if name == "SetEngineClipName" or name == "SetEngineIdleClipName" then
+				local clipNameArgument = variable.Arguments[1]
+				if clipNameArgument.Value == "tt" then
+					clipNameArgument.Value = "snake_car"
+				end
+			elseif name == "SetOverlayClipName" then
+				local clipName = variable.Arguments[1].Value
+				if clipName == "" or clipName == "generator" then
+					carSoundParameters:RemoveVariable(index)
 				end
 			end
 		end
--- Randomiser/Resources/lib/SPT.lua
+++ Randomiser/Resources/lib/SPT.lua
@@ -7,6 +7,7 @@ local string_gmatch = string.gmatch
 local string_match = string.match
 
 local table_concat = table.concat
+local table_remove = table.remove
 
 SPT = {}
 
@@ -41,6 +42,47 @@ local function LoadSPTFile(self, Path)
 end
 
 SPT.SPTFile = setmetatable({load = LoadSPTFile, new = LoadSPTFile, LoadFromData = LoadSPTFromData}, {__call = LoadSPTFile})
+
+function SPT.SPTFile:GetClasses(Type, Backwards, Name)
+	assert(Type == nil or type(Type) == "string", "Arg #1 (Type) must be a string.")
+	assert(Name == nil or type(Name) == "string", "Arg #3 (Name) must be a string.")
+	
+	local classes = self.Classes
+	local classesN = #classes
+	
+	local index, finish, step
+	if Backwards then
+		index = classesN
+		finish = 0
+		step = -1
+	else
+		index = 1
+		finish = classesN + 1
+		step = 1
+	end
+	
+	return function()
+		while index ~= finish do
+			assert(Backwards or classesN == #classes, string_format("Class count changed from %d to %d. To add or remove classes whilst iterating, you must iterate backwards", classesN, #classes))
+			local Class = classes[index]
+			index = index + step
+			if (Type == nil or Class.Type == Type) and (Name == nil or Class.Name == Name) then
+				return Class, index - step
+			end
+		end
+		return nil
+	end
+end
+
+function SPT.SPTFile:GetClass(Type, Backwards, Name)
+	return self:GetClasses(Type, Backwards, Name)()
+end
+
+function SPT.SPTFile:RemoveClass(Index)
+	assert(type(Index) == "number", "Arg #1 (Index) must be a number.")
+	assert(Index > 0 and Index <= #self.Classes, "Arg #1 (Index) out of bounds.")
+	table_remove(self.Classes, Index)
+end
 
 function SPT.SPTFile:__tostring()
 	local Classes = self.Classes
@@ -132,6 +174,46 @@ SPT.Class = setmetatable({}, {
 		return setmetatable(Data, self)
 	end,
 })
+
+function SPT.Class:GetVariables(Backwards, Name)
+	assert(Name == nil or type(Name) == "string", "Arg #2 (Name) must be a string.")
+	
+	local variables = self.Variables
+	local variablesN = #variables
+	
+	local index, finish, step
+	if Backwards then
+		index = variablesN
+		finish = 0
+		step = -1
+	else
+		index = 1
+		finish = variablesN + 1
+		step = 1
+	end
+	
+	return function()
+		while index ~= finish do
+			assert(Backwards or variablesN == #variables, string_format("Variable count changed from %d to %d. To add or remove variables whilst iterating, you must iterate backwards", variablesN, #variables))
+			local Variable = variables[index]
+			index = index + step
+			if Name == nil or Variable.Name == Name then
+				return Variable, index - step
+			end
+		end
+		return nil
+	end
+end
+
+function SPT.Class:GetVariable(Backwards, Name)
+	return self:GetVariables(Backwards, Name)()
+end
+
+function SPT.Class:RemoveVariable(Index)
+	assert(type(Index) == "number", "Arg #1 (Index) must be a number.")
+	assert(Index > 0 and Index <= #self.Variables, "Arg #1 (Index) out of bounds.")
+	table_remove(self.Variables, Index)
+end
 
 function SPT.Class:__tostring()
 	local data = {}
