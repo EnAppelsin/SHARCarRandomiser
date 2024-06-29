dc97284ba43f6acdc59af57cc4f8d7e51079c5e0
Proddy <josh@zhbot.org>
2024-06-29 03:10:16 +0100

    Added `MissionVehicles`

---
+++ Randomiser/Resources/Modules/Vehicles/MissionVehicles.lua
@@ -0,0 +1,129 @@
+local math_random = math.random
+local table_remove = table.remove
+
+local RandomMissionVehicles = Module("Random Mission Vehicles", "RandomMissionVehicles", 3)
+
+RandomMissionVehicles:AddMissionHandler(function(LevelNumber, MissionNumber, MissionLoad, MissionInit)
+	-- TODO: Save on reload if some setting enabled or something
+	
+	local RandomVehicleIndex = math_random(CarCount)
+	local RandomVehicleP3D = CarP3DFiles[RandomVehicleIndex]
+	local RandomVehicleName = CarNames[RandomVehicleIndex]
+	
+	print("Setting mission vehicle to: " .. RandomVehicleName)
+	
+	local isForced = false
+	local functions = MissionLoad.Functions
+	
+	for i=1,#functions do
+		local func = functions[i]
+		local name = func.Name:lower()
+		if name == "loaddisposablecar" and func.Arguments[3] == "OTHER" then
+			isForced = true
+			func.Arguments[1] = RandomVehicleP3D
+			func.Arguments[2] = RandomVehicleName
+			break
+		end
+	end
+	
+	if isForced then
+		MissionInit:SetAll("InitLevelPlayerVehicle", 1, RandomVehicleName)
+	else
+		MissionLoad:AddFunction("LoadDisposableCar", {RandomVehicleP3D, RandomVehicleName, "OTHER"})
+		
+		local CarLocator
+		local LastStageIndex
+		local ResetToHereIndex
+		local functions = MissionInit.Functions
+		for i=1,#functions do
+			local func = functions[i]
+			local name = func.Name:lower()
+			
+			if name == "setmissionresetplayeroutcar" then
+				CarLocator = func.Arguments[2]
+			elseif name == "setmissionresetplayerincar" then
+				CarLocator = func.Arguments[1]
+			elseif name == "addstage" then
+				LastStageIndex = i
+			elseif name == "reset_to_here" then
+				ResetToHereIndex = LastStageIndex
+			end
+			
+			if CarLocator and ResetToHereIndex then
+				break
+			end
+		end
+		if not CarLocator then
+			-- TODO: Is this even possible?
+			print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+		end
+		MissionInit:InsertFunction(2, "SetForcedCar")
+		MissionInit:InsertFunction(2, "InitLevelPlayerVehicle", {RandomVehicleName, CarLocator, "OTHER"})
+		
+		if ResetToHereIndex then
+			local remove = false
+			for i=ResetToHereIndex+1,1,-1 do -- We want the function before AddStage. We've inserted 2. Add 1.
+				local func = functions[i]
+				local name = func.Name:lower()
+				
+				print(name)
+				
+				if name == "closestage" then
+					remove = true
+				end
+				
+				if remove then
+					table_remove(functions, i)
+				end
+				
+				if name == "addstage" then
+					remove = false
+				end
+			end
+		end
+		
+		if Settings.RemoveOutOfVehicle then
+			local toRemove = {}
+			local toRemoveN = 0
+			local addCondition
+			local remove = false
+			for i=1,#functions do
+				local func = functions[i]
+				local name = func.Name:lower()
+				
+				if name == "addcondition" then
+					if func.Arguments[1] == "damage" then
+						addCondition = i
+					end
+				end
+				
+				if addCondition then
+					if name == "setcondtargetvehicle" then
+						if func.Arguments[1] == RandomVehicleName then -- TODO: Check if "current" is valid
+							remove = true
+						end
+					end
+					
+					if name == "closecondition" then
+						if remove then
+							for j=addCondition,i do
+								toRemoveN = toRemoveN + 1
+								toRemove[toRemoveN] = j
+							end
+						end
+						remove = false
+						addCondition = nil
+					end
+				end
+			end
+			
+			for i=toRemoveN,1,-1 do
+				table_remove(functions, toRemove[i])
+			end
+		end
+	end
+	
+	return true
+end)
+
+return RandomMissionVehicles
