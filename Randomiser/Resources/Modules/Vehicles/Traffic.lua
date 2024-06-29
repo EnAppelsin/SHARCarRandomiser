f57799694b190f578df4f4e187052b054530c848
Proddy <josh@zhbot.org>
2024-06-29 02:23:41 +0100

    Updated `RandomTraffic` to respect already loaded cars

--- Randomiser/Resources/Modules/Vehicles/Traffic.lua
+++ Randomiser/Resources/Modules/Vehicles/Traffic.lua
@@ -6,7 +6,7 @@ local table_unpack = table.unpack
 local GetFileName = GetFileName
 local RemoveFileExtension = RemoveFileExtension
 
-local RandomTraffic = Module("Random Traffic", "RandomTraffic")
+local RandomTraffic = Module("Random Traffic", "RandomTraffic", 2)
 
 RandomTraffic:AddLevelHandler(function(LevelNumber, LevelLoad, LevelInit)
 	local carP3DPool = {table_unpack(CarP3DFiles)}
@@ -33,7 +33,7 @@ RandomTraffic:AddLevelHandler(function(LevelNumber, LevelLoad, LevelInit)
 				table_remove(carP3DPool, randomCarIndex)
 				table_remove(carNamePool, randomCarIndex)
 				
-				addedTraffic[randomCarP3D] = true
+				addedTraffic[randomCarP3D] = randomCarName
 				
 				local args = {randomCarName, 1}
 				if math.random(3) == 1 then
@@ -51,18 +51,30 @@ RandomTraffic:AddLevelHandler(function(LevelNumber, LevelLoad, LevelInit)
 		end
 	end
 	
+	local loadedCars = {}
 	functions = LevelLoad.Functions
 	for i=#functions,1,-1 do
 		local func = functions[i]
-		if func.Name:lower() == "loadp3dfile" and string_match(func.Arguments[1], "^art[\\/]cars[\\/]") then
-			local carName = string_match(func.Arguments[1], "^art[\\/]cars[\\/]([^\\/]+)%.p3d")
-			if carName ~= nil and chaseCar ~= carName and removedTraffic[carName] then
-				table_remove(functions, i)
+		if func.Name:lower() == "loadp3dfile" then
+			local carName = string_match(func.Arguments[1], "art[\\/]cars[\\/]([^\\/]+)%.p3d")
+			if carName ~= nil then
+				if chaseCar ~= carName and removedTraffic[carName] then
+					table_remove(functions, i)
+				else
+					loadedCars[carName] = true
+				end
+			end
+		elseif func.Name:lower() == "loaddisposablecar" then
+			local carName = string_match(func.Arguments[1], "art[\\/]cars[\\/]([^\\/]+)%.p3d")
+			if carName then
+				loadedCars[carName] = true
 			end
 		end
 	end
-	for k in pairs(addedTraffic) do
-		LevelLoad:AddFunction("LoadP3DFile", k)
+	for k,v in pairs(addedTraffic) do
+		if not loadedCars[v] then
+			LevelLoad:AddFunction("LoadP3DFile", k)
+		end
 	end
 	
 	return true
