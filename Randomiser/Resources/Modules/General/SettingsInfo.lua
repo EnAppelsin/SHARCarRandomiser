37adaa99ae4e7bbf9360288b384bdf204039c1de
Proddy <josh@zhbot.org>
2024-07-03 20:05:23 +0100

    Update SettingsInfo.lua

--- Randomiser/Resources/Modules/General/SettingsInfo.lua
+++ Randomiser/Resources/Modules/General/SettingsInfo.lua
@@ -24,6 +24,7 @@ local AudioVisualSettings = {
 	[1 << 4] = Settings.RandomMissionCharacters,
 	[1 << 5] = Settings.RandomDrivers,
 	[1 << 6] = Settings.RandomMusic,
+	[1 << 6] = Settings.RandomCarSounds,
 }
 
 local ChaosSettings = {
