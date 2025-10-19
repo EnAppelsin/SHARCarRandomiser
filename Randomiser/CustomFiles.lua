92cf7d56b40544a230f2f6e242b64c34af9f9ad2
EnAppelsin <39798849+EnAppelsin@users.noreply.github.com>
2025-10-19 18:17:14 +0200

    Move pool checks to after custom framework loading

--- Randomiser/CustomFiles.lua
+++ Randomiser/CustomFiles.lua
@@ -76,20 +76,6 @@ end
 
 DebugPrint("Debug settings enabled: " .. (Settings.UseDebugSettings and "true" or "false"))
 
-if #RandomCarPoolPlayer < 5 and Settings.RandomPlayerVehicles then
-	Alert("You have chosen less than 5 cars for the random player pool. You must choose at least 5 cars.")
-	os.exit()
-elseif #RandomCarPoolTraffic < 5 and Settings.RandomTraffic then
-	Alert("You have chosen less than 5 cars for the random traffic pool. You must choose at least 5 cars.")
-	os.exit()
-elseif #RandomCarPoolMission < 5 and Settings.RandomMissionVehicles then
-	Alert("You have chosen less than 5 cars for the random mission pool. You must choose at least 5 cars.")
-	os.exit()
-elseif #RandomCarPoolChase < 5 and Settings.RandomChase then
-	Alert("You have chosen less than 5 cars for the random chase pool. You must choose at least 5 cars.")
-	os.exit()
-end
-
 if Settings.VerboseDebug then
 	local OldOutput = Output
 	local OldRedirect = Redirect
@@ -122,6 +108,20 @@ end
 dofile(Paths.Resources .. "RandomCarTune.lua")
 if Settings.RandomDialogue then
 	dofile(Paths.Resources .. "RandomDialogue.lua")
+end
+
+if #RandomCarPoolPlayer < 5 and Settings.RandomPlayerVehicles then
+	Alert("You have chosen less than 5 cars for the random player pool. You must choose at least 5 cars.")
+	os.exit()
+elseif #RandomCarPoolTraffic < 5 and Settings.RandomTraffic then
+	Alert("You have chosen less than 5 cars for the random traffic pool. You must choose at least 5 cars.")
+	os.exit()
+elseif #RandomCarPoolMission < 5 and Settings.RandomMissionVehicles then
+	Alert("You have chosen less than 5 cars for the random mission pool. You must choose at least 5 cars.")
+	os.exit()
+elseif #RandomCarPoolChase < 5 and Settings.RandomChase then
+	Alert("You have chosen less than 5 cars for the random chase pool. You must choose at least 5 cars.")
+	os.exit()
 end
 
 DebugPrint("Loaded " .. #RandomCarPoolPlayer .. " cars for the random Player pool")
